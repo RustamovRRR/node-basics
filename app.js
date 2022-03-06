f2c3b2ebd36fa15fc0b55d122cc6ee19061d9533
RustamovRRR <rustamrrustamov@gmail.com>
2022-03-07 00:26:59 +0400

    sending response

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const http = require("http");
 
 // create server
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.headers, req.method);
   //   process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Node Page</title></head>");
+  res.write("<body><h1>Hello from Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
