0847e3ff9a45a41e343b790a95c61c3d27dde910
RustamovRRR <rustamrrustamov@gmail.com>
2022-03-07 21:15:26 +0400

    routing requests

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const http = require("http");
 
 // create server
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My Node Page</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
   //   process.exit();
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
