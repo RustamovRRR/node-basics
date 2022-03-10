6be9c1a6a1dfdfd86091cf4b4a5d8680062a6641
RustamovRRR <rustamrrustamov@gmail.com>
2022-03-10 23:32:15 +0400

    installing express,js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // importing http module
 const http = require("http");
+const express = require("express");
+
+const app = express();
 
 // create server
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(3000);
