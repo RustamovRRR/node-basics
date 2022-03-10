fc7dc00dfafedb1d3f56da389963a07edeab7681
RustamovRRR <rustamrrustamov@gmail.com>
2022-03-10 23:41:39 +0400

    adding middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("first middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("second middleware");
+  //   next();
+});
+
 // create server
 const server = http.createServer(app);
 
